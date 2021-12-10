19d68c567a983cb6ca3711bb9dc0e915f1b6aee7
Connor O'Hara <connor@switchboard.xyz>
2021-12-09 19:35:56 -0500

    hooray for working ghost addressing

--- src/client.rs
+++ src/client.rs
@@ -1,23 +1,313 @@
 use futures::stream::{SplitStream, SplitSink};
-use sodiumoxide::crypto::box_::{PublicKey as SodiumPublicKey, SecretKey as SodiumSecretKey};
+use futures::SinkExt;
+
+use sodiumoxide::crypto::box_::{PublicKey as SodiumPublicKey, SecretKey as SodiumSecretKey, Nonce as SodiumNonce};
+use sodiumoxide::crypto::box_;
+
 use flurry::HashMap;
+
 use std::{
     sync::{Arc, Mutex},
     net::SocketAddr,
 };
+
+use async_std::task;
 use async_std::net::{TcpStream};
 use async_tungstenite::tungstenite::Message;
 use async_tungstenite::WebSocketStream;
 
-#[derive(Clone, Debug)]
+use ghostmates_common::{GhostmatesMessage,ProtocolMessage, PCheckMessage};
+use colored::Colorize;
+use base58::{ToBase58, FromBase58};
+use sha2::{Sha256, Digest, 
+    digest::generic_array::GenericArray
+};
+use ripemd160::{Ripemd160};
+use bincode;
+
+pub struct SenderPCheckProtoData {}
+pub struct CourierPCheckProtoData {}
+pub struct RecipientPCheckProtoData {}
+pub struct ACheckProtoData {}
+
+#[derive(Clone)]
 pub struct Client {
     pk: SodiumPublicKey,
     sk: SodiumSecretKey,
     server_write: Arc<Mutex<SplitSink<WebSocketStream<TcpStream>, Message>>>,
-    peer_store: Arc<HashMap<String, SodiumPublicKey>>
+
+    peer_store: Arc<HashMap<String, SodiumPublicKey>>,
+    sender_pcheck_table: Arc<HashMap<(String, String), SenderPCheckProtoData>>,
+    courier_pcheck_table: Arc<HashMap<(String, String), CourierPCheckProtoData>>,
+    recipient_pcheck_table: Arc<HashMap<(String, String), RecipientPCheckProtoData>>,
+
+    outgoing_encrypts: Arc<HashMap<String, Arc<Mutex<Vec<ProtocolMessage>>>>>,
+    incoming_decrypts: Arc<HashMap<String, Arc<Mutex<Vec<(SodiumNonce, Vec<u8>)>>>>>,
+
+    accepting_achecks: bool,
+    pcheck_courier: bool,
+    pcheck_sender: bool,
+    pcheck_recipient: bool,
+}
+
+pub fn new_client(pk: SodiumPublicKey, 
+    sk: SodiumSecretKey, 
+    server_write: Arc<Mutex<SplitSink<WebSocketStream<TcpStream>, Message>>>,
+) -> Client {
+    Client {
+        pk: pk,
+        sk: sk,
+        server_write: server_write,
+
+        sender_pcheck_table: Arc::new(HashMap::new()),
+        courier_pcheck_table: Arc::new(HashMap::new()),
+        recipient_pcheck_table: Arc::new(HashMap::new()),
+        outgoing_encrypts: Arc::new(HashMap::new()),
+        incoming_decrypts: Arc::new(HashMap::new()),
+        peer_store: Arc::new(HashMap::new()),
+
+        accepting_achecks: true,
+        pcheck_courier: true,
+        pcheck_sender: true,
+        pcheck_recipient: true,
+    }
 }
 
 impl Client {
-    pub fn validate_address(&GhostmatesMessage) {
+    pub fn route(&self, gm: &GhostmatesMessage) {
+        match gm {
+            GhostmatesMessage::SuccesfulIdentify => {
+                print!("{}\nghxc> ", "Succesfully Authenticated".green());
+            },
+            GhostmatesMessage::FailedIdentify => {
+                print!("{}\nghxc> ", "Failed to authenticate!".red());
+            },
+            GhostmatesMessage::SuccesfulLookup {
+                ghostmates_address,
+                pubkey
+            }=> {
+                if self.validate_address(ghostmates_address.clone(), pubkey.clone()) {
+                    if let Some(to_decrypt) = self.incoming_decrypts.get(
+                        &ghostmates_address.clone(),
+                        &self.incoming_decrypts.guard()
+                    ) {
+                        to_decrypt.lock().unwrap().iter().for_each(|(nonce, cyphertext)| {
+                            self.decrypt_incoming(ghostmates_address.clone(), cyphertext.to_vec(), *nonce);
+                        });
+                    }
+                    if let Some(to_encrypt) = self.outgoing_encrypts.get(
+                        &ghostmates_address.clone(),
+                        &self.outgoing_encrypts.guard()
+                    ) {
+                        to_encrypt.lock().unwrap().iter().for_each(|pm| {
+                            if let Some(dm) = self.direct_message(self.sk.clone(), ghostmates_address.clone(), pm.clone(), self.peer_store.clone()) {
+                                let serialized_dm = bincode::serialize(&dm)
+                                    .expect("Couldn't serialize dm");
+                                task::block_on(
+                                    self.server_write
+                                    .lock()
+                                    .unwrap()
+                                    .send(
+                                        Message::Binary(serialized_dm)
+                                    )
+                                );
+                            }
+                            else {
+                                print!("Major problem dude.\nghxc> ");
+                            }
+                        });
+                    }
+                }
+                else {
+                    println!("Not good!");
+                }
+            },
+            GhostmatesMessage::FailedLookup {
+                ghostmates_address
+            } => {
+                print!("{}\nghxc> ", format!("invalid lookup for {}", ghostmates_address).red());
+            },
+            GhostmatesMessage::IncomingMessage {
+                from_address,
+                encrypted_message,
+                nonce,
+            } => {
+                self.decrypt_incoming(from_address.clone(), encrypted_message.clone(), nonce.clone());
+            },
+            _ => {
+                print!("{}\nghxc> ", "Got an unknown response".red());
+            }
+        }
     }
+
+    pub fn validate_address(&self, ghostmates_address: String, pubkey: SodiumPublicKey) -> bool {
+        let address_from_pk = address_from_sodium_pk(&pubkey);
+        if address_from_pk.eq(&ghostmates_address.clone()) {
+            print!("{}\nghxc> ", "valid!!!".green());
+            if !self.peer_store.contains_key(&ghostmates_address.clone(), &self.peer_store.guard()) {
+                print!("\n{}\nghxc> ", "Added to peerstore.".yellow());
+                self.peer_store.insert(ghostmates_address.clone(), pubkey.clone(), &self.peer_store.guard());
+            }
+            return true
+        }
+        else {
+            print!("{}\nghxc> ", "invalid!!! (This should never happen)".red());
+            return false
+        }
+
+    }
+
+    pub fn decrypt_incoming(&self, from_address: String, encrypted_message: Vec<u8>, nonce: SodiumNonce) {
+        print!("{}",
+            format!("{} {}\nghxc> ", "Received a message from".yellow(), from_address.blue())
+        );
+        if let Some(their_pk) = self.peer_store.get(
+            &from_address.clone(), 
+            &self.peer_store.guard()
+        ) {
+            let decrypted = box_::open(&encrypted_message, &nonce, &their_pk, &self.sk)
+                .expect("Could not decrypt.");
+            let deserialized : ProtocolMessage = bincode::deserialize(&decrypted)
+                .expect("Couldn't deserialize.");
+            self.route_incoming(from_address.clone(), deserialized.clone());
+        }
+        else {
+            print!("{} {}\nghxc> ", "We don't have their PK.".red(), "adding their message to the decrypt queue and looking up their PK...");
+            if let Some(decrypt_vector) = self.incoming_decrypts.get(
+                &from_address.clone(),
+                &self.incoming_decrypts.guard()
+            ) {
+                decrypt_vector.lock().unwrap().push((nonce, encrypted_message));
+            }
+            else {
+                self.incoming_decrypts.insert(
+                    from_address.clone(), 
+                    Arc::new(Mutex::new(vec![(
+                        nonce,
+                        encrypted_message
+                    )])), 
+                    &self.incoming_decrypts.guard()
+                );
+            }
+            self.lookup(from_address);
+        }
+
+    }
+
+    pub fn pcheck(&self, courier_address: String, recipient_address: String) {
+
+        if ! (courier_address.len() > 31 && courier_address.len() < 35) {
+            print!("{}\n", "invalid courier addres".red());
+            return
+        }
+        if ! (recipient_address.len() > 31 && recipient_address.len() < 35) {
+            print!("{}\n", "invalid recipient address".red());
+            return
+        }
+        let intro = ProtocolMessage::PCheck(
+            PCheckMessage::RequestForPCheck {
+                recipient_ghost_address: recipient_address.clone(),
+            }
+        );
+        if let Some(dm) = self.direct_message(self.sk.clone(), recipient_address.clone(), intro, self.peer_store.clone()) {
+            let serialized_dm = bincode::serialize(&dm)
+                .expect("Couldn't serialize dm");
+            task::block_on(
+                self.server_write
+                .lock()
+                .unwrap()
+                .send(
+                    Message::Binary(serialized_dm)
+                )
+            );
+        }
+    }
+
+    pub fn lookup(&self, lookup_address: String) {
+
+        let lookup = GhostmatesMessage::Lookup {
+            dest_address: lookup_address
+        };
+        let lookup_as_bytes: Vec<u8> = bincode::serialize(&lookup)
+            .expect("Could not serialize lookup message");
+        task::block_on(
+            self.server_write
+            .lock()
+            .unwrap()
+            .send(
+                Message::Binary(lookup_as_bytes)
+            )
+        );
+    }
+
+    pub fn toggle(&self, arg: &str) {
+        println!("This doesn't do anything for now.");
+        match arg {
+            "neighbor" => {
+            },
+            "courier" => {
+
+            },
+            "recipient" => {
+
+            },
+            _ => {
+
+            }
+        }
+    }
+
+    pub fn direct_message(&self, sk: SodiumSecretKey, ghost_address: String, protocol_message: ProtocolMessage, peer_store: Arc<HashMap<String, SodiumPublicKey>>) -> Option<GhostmatesMessage> {
+        //let pubkey = *peer_store.get(&ghost_address, &peer_store.guard()).unwrap();
+        if let Some(pubkey) = peer_store.get(&ghost_address, &peer_store.guard()) {
+            let nonce = box_::gen_nonce();
+            let serialized_message = bincode::serialize(&protocol_message)
+                .expect("Could not serialize protocol message");
+            let cyphertext = box_::seal(&serialized_message, &nonce, &pubkey, &sk);
+            Some(GhostmatesMessage::DirectMessage{
+                dest_address: ghost_address,
+                encrypted_message: cyphertext,
+                nonce: nonce,
+            })
+        }
+        else {
+            if let Some(encrypt_vector) = self.outgoing_encrypts.get(
+                &ghost_address.clone(),
+                &self.incoming_decrypts.guard()
+            ) {
+                encrypt_vector.lock().unwrap().push(protocol_message);
+            }
+            else {
+                self.outgoing_encrypts.insert(
+                    ghost_address.clone(), 
+                    Arc::new(Mutex::new(vec![
+                        protocol_message
+                    ])), 
+                    &self.incoming_decrypts.guard()
+                );
+            }
+            self.lookup(ghost_address);
+            None
+        }
+    }
+
+    pub fn route_incoming(&self, from_address: String, message: ProtocolMessage) {
+        println!("{} {:?} from {}", "received".bold(), message, from_address.blue());
+    }
+
 }
+
+pub fn address_from_sodium_pk(pk: &SodiumPublicKey) -> String {
+    let mut hasher = Sha256::new();
+    hasher.update(pk.as_ref());
+    let result = hasher.finalize();
+    let sha256hash : Vec<u8> = result.as_slice().to_owned();
+    let mut ripemd_hasher = Ripemd160::new();
+    ripemd_hasher.update(sha256hash);
+    let ripemd_result = ripemd_hasher.finalize();
+    let ripemdhash: Vec<u8> = ripemd_result.as_slice().to_owned(); 
+    let mut base58 = ripemdhash.to_base58();
+    base58.push_str(".ghost");
+    base58
+}
+
--- src/main.rs
+++ src/main.rs
@@ -40,7 +40,14 @@ use serde::{Serialize, Deserialize};
 
 use flurry::HashMap;
 
+use ghostmates_common::{
+    GhostmatesMessage, 
+    ProtocolMessage, 
+    PCheckMessage,
+};
+
 mod client;
+use client::{address_from_sodium_pk, Client, new_client};
 
 type WebSocketWrite = SplitSink<WebSocketStream<TcpStream>, Message>;
 type WebSocketRead = SplitStream<WebSocketStream<TcpStream>>;
@@ -58,7 +65,7 @@ async fn connect_to_server() -> WebSocketStream<TcpStream> {
 
 }
 
-async fn server_loop(read: WebSocketRead, s: Arc<Mutex<WebSocketWrite>>, peer_store: Arc<HashMap<String, SodiumPublicKey>>, sk: SodiumSecretKey) {
+async fn server_loop(read: WebSocketRead, client: Client) {
 
     let _server_incoming = read
         .try_filter(|msg| {
@@ -67,57 +74,7 @@ async fn server_loop(read: WebSocketRead, s: Arc<Mutex<WebSocketWrite>>, peer_st
         .try_for_each(|msg| {
             if let Message::Binary(d) = msg {
                 let gm : GhostmatesMessage = bincode::deserialize(&d).unwrap();
-                match gm {
-                    GhostmatesMessage::SuccesfulIdentify => {
-                        print!("{}\nghxc> ", "Succesfully Authenticated".green());
-                    },
-                    GhostmatesMessage::FailedIdentify => {
-                        print!("{}\nghxc> ", "Failed to authenticate!".red());
-                    },
-                    GhostmatesMessage::SuccesfulLookup {
-                        ghostmates_address,
-                        pubkey,
-                    } => {
-                        let address_from_pk = address_from_sodium_pk(&pubkey);
-                        if address_from_pk.eq(&ghostmates_address) {
-                            print!("{}\nghxc> ", "valid!!!".green());
-                            if !peer_store.contains_key(&ghostmates_address, &peer_store.guard()) {
-                                print!("\n{}\nghxc> ", "Added to peerstore.".yellow());
-                                peer_store.insert(ghostmates_address, pubkey, &peer_store.guard());
-                            }
-                        }
-                        else {
-                            print!("{}\nghxc> ", "invalid!!! (This should never happen)".red());
-                        }
-                    },
-                    GhostmatesMessage::FailedLookup {
-                        ghostmates_address
-                    } => {
-                        print!("{}\nghxc> ", format!("invalid lookup for {}", ghostmates_address).red());
-                    },
-                    GhostmatesMessage::IncomingMessage {
-                        from_address,
-                        encrypted_message,
-                        nonce,
-                    } => {
-                        print!("{}",
-                            format!("{} {}\nghxc> ", "Received a message from".yellow(), from_address.blue())
-                        );
-                        if let Some(their_pk) = peer_store.get(&from_address, &peer_store.guard()) {
-                            let decrypted = box_::open(&encrypted_message, &nonce, &their_pk, &sk)
-                                .expect("Could not decrypt.");
-                            let deserialized : ProtocolMessage = bincode::deserialize(&decrypted)
-                                .expect("Couldn't deserialize.");
-                            println!("deserialized: {:?}", deserialized);
-                        }
-                        else {
-                            println!("{}", "We don't have their PK. bummer.".red());
-                        }
-                    },
-                    _ => {
-                        print!("{}\nghxc> ", "Got an unknown response".red());
-                    }
-                }
+                client.route(&gm);
             }
             stdout().flush().unwrap();
             future::ok(())
@@ -126,7 +83,7 @@ async fn server_loop(read: WebSocketRead, s: Arc<Mutex<WebSocketWrite>>, peer_st
     println!("Server loop ended.");
 }
 
-async fn user_loop(s: Arc<Mutex<WebSocketWrite>>, peer_store: Arc<HashMap<String, SodiumPublicKey>>, my_key: SodiumSecretKey) {
+async fn user_loop(client: Client) {
     loop {
         let mut input_string = String::new();
         stdout().flush().unwrap();
@@ -143,65 +100,23 @@ async fn user_loop(s: Arc<Mutex<WebSocketWrite>>, peer_store: Arc<HashMap<String
         match split_string[0] {
             "pcheck" => {
                 if split_string.len() == 3 {
-                    let first_gaddress = split_string[1];
-                    let second_gaddress = split_string[2];
-                    if ! (first_gaddress.len() > 31 && first_gaddress.len() < 35) {
-                        print!("{}\n", "invalid first address".red());
-                        continue
-                    }
-                    if ! (second_gaddress.len() > 31 && second_gaddress.len() < 35) {
-                        print!("{}\n", "invalid second address".red());
-                        continue
-                    }
-                    let intro = ProtocolMessage::PCheck(
-                        PCheckMessage::RequestForPCheck {
-                            recipient_ghost_address: second_gaddress.to_string()
-                        }
-                    );
-                    let dm = direct_message(my_key.clone(), second_gaddress.to_string(), intro, peer_store.clone());
-                    let serialized_dm = bincode::serialize(&dm)
-                        .expect("Couldn't serialize dm");
-                    task::block_on(
-                        s
-                            .lock()
-                            .unwrap()
-                            .send(
-                                Message::Binary(serialized_dm)
-                            )
-                    );
+                    let first_gaddress = split_string[1].to_string();
+                    let second_gaddress = split_string[2].to_string();
+                    client.pcheck(first_gaddress, second_gaddress);
                 }
                 else {
                     print!("{}\n", "Invalid".red());
                     continue
                 }
+
             },
             "id" => {
-                let lookup = GhostmatesMessage::Lookup {
-                    dest_address: split_string[1].to_string()
-                };
-                let lookup_as_bytes: Vec<u8> = bincode::serialize(&lookup)
-                    .expect("Could not serialize lookup message");
-                task::block_on(
-                    s
-                        .lock()
-                        .unwrap()
-                        .send(
-                            Message::Binary(lookup_as_bytes)
-                        )
-                );
+                let address_to_lookup = split_string[1].to_string();
+                client.lookup(address_to_lookup);
             },
-            /*"toggle" => {
-                match split_string[1] {
-                    "neighbor" => {
-                    },
-                    "courier" => {
-
-                    },
-                    "recipient" => {
-
-                    },
-                }
-            },*/
+            "toggle" => {
+                client.toggle(split_string[1]);
+            },
             _ => {
 
             }
@@ -209,75 +124,6 @@ async fn user_loop(s: Arc<Mutex<WebSocketWrite>>, peer_store: Arc<HashMap<String
     }
 }
 
-pub fn address_from_sodium_pk(pk: &SodiumPublicKey) -> String {
-    let mut hasher = Sha256::new();
-    hasher.update(pk.as_ref());
-    let result = hasher.finalize();
-    let sha256hash : Vec<u8> = result.as_slice().to_owned();
-    let mut ripemd_hasher = Ripemd160::new();
-    ripemd_hasher.update(sha256hash);
-    let ripemd_result = ripemd_hasher.finalize();
-    let ripemdhash: Vec<u8> = ripemd_result.as_slice().to_owned(); 
-    let mut base58 = ripemdhash.to_base58();
-    base58.push_str(".ghost");
-    base58
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-enum GhostmatesMessage {
-    Identify {
-        ghostmates_address: String,
-        pubkey: SodiumPublicKey,
-    },
-    SuccesfulIdentify,
-    FailedIdentify,
-    SuccesfulLookup {
-        pubkey: SodiumPublicKey,
-        ghostmates_address: String,
-    },
-    FailedLookup {
-        ghostmates_address: String,
-    },
-    Lookup {
-        dest_address: String,
-    },
-    DirectMessage {
-        dest_address: String,
-        encrypted_message: Vec<u8>,
-        nonce: SodiumNonce
-    },
-    IncomingMessage {
-        from_address: String,
-        encrypted_message: Vec<u8>,
-        nonce: SodiumNonce,
-    },
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-enum PCheckMessage {
-    RequestForPCheck {
-        recipient_ghost_address: String
-    }
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-enum ProtocolMessage {
-    ACheck,
-    PCheck(PCheckMessage),
-}
-
-fn direct_message(sk: SodiumSecretKey, ghost_address: String, protocol_message: ProtocolMessage, peer_store: Arc<HashMap<String, SodiumPublicKey>>) -> GhostmatesMessage {
-    let pubkey = *peer_store.get(&ghost_address, &peer_store.guard()).unwrap();
-    let nonce = box_::gen_nonce();
-    let serialized_message = bincode::serialize(&protocol_message)
-        .expect("Could not serialize protocol message");
-    let cyphertext = box_::seal(&serialized_message, &nonce, &pubkey, &sk);
-    GhostmatesMessage::DirectMessage{
-        dest_address: ghost_address,
-        encrypted_message: cyphertext,
-        nonce: nonce,
-    }
-}
 
 enum ActiveProtocolRow {
     ACheck {
@@ -322,7 +168,6 @@ fn main() {
     let ghost_address = address_from_sodium_pk(&ourpk);
     println!("{} {}", "Your address: ".bold(), &ghost_address);
 
-    let peer_store = Arc::new(HashMap::<String, SodiumPublicKey>::new());
     let active_protocols = Arc::new(HashMap::<ActiveProtocolRow, ProtocolRoundData>::new());
 
     let id_message = GhostmatesMessage::Identify {
@@ -335,6 +180,7 @@ fn main() {
     let server_connection = task::block_on(connect_to_server());
     let (server_write, server_read) = server_connection.split();
     let arc_server_write = Arc::new(Mutex::new(server_write));
+    let client = new_client(ourpk.clone(), oursk.clone(), arc_server_write.clone());
     task::block_on(
         arc_server_write
             .lock()
@@ -343,6 +189,6 @@ fn main() {
                 Message::Binary(id_message_as_bytes)
             )
     );
-    task::spawn(server_loop(server_read, arc_server_write.clone(), peer_store.clone(), oursk.clone()));
-    task::block_on(user_loop(arc_server_write.clone(), peer_store.clone(), oursk.clone()));
+    task::spawn(server_loop(server_read, client.clone()));
+    task::block_on(user_loop(client.clone()));
 }
